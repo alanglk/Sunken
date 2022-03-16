1c738b85a3d05c50533e152ac9bebfb540fe77f8
Danelitos <danel.alonso10@gmail.com>
2022-03-16 12:17:47 +0100

    Prueba

--- src/org/ProgramaPrincipal.java
+++ src/org/ProgramaPrincipal.java
@@ -3,6 +3,6 @@ package org;
 public class ProgramaPrincipal {
 
     public static void main(String[] args){
-        System.out.println("--- SUNKEN ---");
+        System.out.println("---SUNKEN---");
     }
 }

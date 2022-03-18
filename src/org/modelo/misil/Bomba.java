cb224e3a859f1de7386663acc090c46353f98944
ALVARO <alvarodiezandino@gmail.com>
2022-03-18 13:09:33 +0100

    Implementacion de misiles y obt area

--- src/org/modelo/misil/Bomba.java
+++ src/org/modelo/misil/Bomba.java
@@ -4,18 +4,19 @@ import org.modelo.misil.Misil;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class Bomba extends Misil {
 
-	/**
-	 * 
-	 * @param pPosicion
-	 * @param pAnchuraTablero
-	 */
-	public ArrayList<Integer> obtArea(int pPosicion, int pAnchuraTablero) {
-
-	}
-
+    /**
+     * @param pPosicion
+     * @param pAnchuraTablero
+     */
+    public ArrayList<Integer> obtArea(int pPosicion) {
+        ArrayList<Integer> listaInt = new ArrayList<Integer>();
+        listaInt.add(pPosicion);
+		return listaInt;
+    }
 
 
 }

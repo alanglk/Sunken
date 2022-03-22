fab650d0f0c57b525b4a60da204fe20d37bce726
Danelitos <danel.alonso10@gmail.com>
2022-03-22 21:12:24 +0100

    Implementación de PanelMisiles

--- src/org/vista/PanelMisiles.java
+++ src/org/vista/PanelMisiles.java
@@ -5,19 +5,21 @@ import java.awt.*;
 
 public class PanelMisiles extends JPanel {
     private JRadioButton botonBomba;
+
     private final ButtonGroup grupoBotonesMisiles=new ButtonGroup();
 
     public PanelMisiles(){
         setLayout(new GridLayout(1,4,0,0));
-        add(getBotonBomba());
+        botonBomba=getBotonRadio("Bomba");
+
+        add(botonBomba);
     }
 
-    private JRadioButton getBotonBomba() {
-        if (botonBomba == null) {
-            botonBomba = new JRadioButton("Bomba");
-            grupoBotonesMisiles.add(botonBomba);
-            botonBomba.setHorizontalAlignment(SwingConstants.CENTER);
-        }
-        return botonBomba;
+    private JRadioButton getBotonRadio(String text) {
+        JRadioButton boton = new JRadioButton(text);
+        grupoBotonesMisiles.add(boton);
+        boton.setHorizontalAlignment(SwingConstants.CENTER);
+
+        return boton;
     }
 }

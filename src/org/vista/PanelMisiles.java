73ddf89b8adff60e12915c70d2defa90f012df4a
Alan <44586947+AliOlixx@users.noreply.github.com>
2022-03-22 19:00:31 +0100

    Vista inicial terminada.

--- src/org/vista/PanelMisiles.java
+++ src/org/vista/PanelMisiles.java
@@ -1,12 +1,14 @@
 package org.vista;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class PanelMisiles extends JFrame {
+public class PanelMisiles extends JPanel {
     private JRadioButton botonBomba;
     private final ButtonGroup grupoBotonesMisiles=new ButtonGroup();
 
     public PanelMisiles(){
+        setLayout(new GridLayout(1,4,0,0));
         add(getBotonBomba());
     }
 
